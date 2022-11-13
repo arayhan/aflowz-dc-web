f48d633613f31300cb70041f3c412acd473ddbc0
arayhan <rayhanprima99@gmail.com>
2022-11-14 03:10:49 +0700

    [CHORE] add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
 				center: true,
 				padding: '1rem',
 			},
+			colors: {
+				primary: {
+					100: '#d4dbe4',
+					200: '#a9b7c9',
+					300: '#7d94ad',
+					400: '#527092',
+					DEFAULT: '#274c77',
+					500: '#274c77',
+					600: '#1f3d5f',
+					700: '#172e47',
+					800: '#101e30',
+					900: '#080f18',
+				},
+			},
 			fontFamily: {
 				primary: ['"Inter"', 'sans-serif'],
 			},
