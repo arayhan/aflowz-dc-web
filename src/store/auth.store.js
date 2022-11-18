c92b5b8ea5f2b08f4ac97e0070aa12b1407fdb1f
arayhan <rayhanprima99@gmail.com>
2022-11-18 08:59:51 +0700

    [FEAT] Auth Login
    - add user role types constant
    - update auth store

--- src/store/auth.store.js
+++ src/store/auth.store.js
@@ -1,12 +1,21 @@
+import { USER_ROLE_TYPES } from '@/utils/constants';
 import create from 'zustand';
 import { devtools } from 'zustand/middleware';
 
 export const useAuthStore = create(
 	devtools((set) => ({
+		isProcessLogin: false,
 		isLoggedIn: false,
+		auth: null,
 
 		authLogin: (values) => {
-			set({ isLoggedIn: true });
+			set({
+				isLoggedIn: true,
+				auth: {
+					name: values.username,
+					role: USER_ROLE_TYPES.ADMIN
+				}
+			});
 		}
 	}))
 );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,6 @@
 export const APP_NAME = process.env.REACT_APP_APP_NAME;
+
+export const USER_ROLE_TYPES = {
+	ADMIN: 'admin',
+	OWNER: 'owner'
+};
