e2a28f7d666d3b7bc95926ef2f5b9348c3f93ba9
arayhan <rayhanprima99@gmail.com>
2022-11-20 00:28:44 +0700

    [FEAT] program list
    - set banner feature height
    - add auth mock data
    - update program list mock data

--- src/components/molecules/BannerFeature/BannerFeature.jsx
+++ src/components/molecules/BannerFeature/BannerFeature.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const BannerFeature = ({ title, description }) => {
 	return (
 		<div className="bg-primary">
-			<div className="container py-28">
+			<div className="container py-20">
 				<div className="space-y-6 text-white">
 					<div className="font-extralight text-5xl">{title}</div>
 					<div className="text-gray-400">{description}</div>
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,14 @@
 import { http } from './http';
+import MOCK_AUTH from './mock/auth.mock';
 
-export const authLogin = async (params) => {
+export const authLogin = (params) => {
 	try {
 		const request = { ...params };
-		const response = await http.post('/user/login', request);
-		return { success: response.data.success, payload: response.data.data };
+		// const response = await http.post('/user/login', request);
+		// return { success: response.data.success, payload: response.data.data };
+		const response = MOCK_AUTH;
+		console.log({ response });
+		return { success: response.success, payload: response.data };
 	} catch (error) {
 		return { success: false, payload: error };
 	}
