cd09fac9bebf1fa642c2aff8072bd4eae6d42d99
arayhan <rayhanprima99@gmail.com>
2022-11-20 01:32:10 +0700

    [CHORE] Home : handle response on HomeBanner

--- src/pages/Home/sections/HomeBanner/HomeBanner.jsx
+++ src/pages/Home/sections/HomeBanner/HomeBanner.jsx
@@ -5,7 +5,7 @@ export const HomeBanner = () => {
 		<div className="bg-primary">
 			<div className="container max-w-screen-lg py-56">
 				<div className="space-y-6 text-white text-center">
-					<div className="font-extralight text-5xl">DEWI CORYATI&apos;S WEB</div>
+					<div className="font-extralight text-4xl lg:text-5xl">DEWI CORYATI&apos;S WEB</div>
 					<div className="text-gray-400">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, praesentium ad odio corrupti asperiores
 						aperiam eaque corporis laborum soluta voluptate atque illo ullam tenetur perspiciatis. At consequatur
