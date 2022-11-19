a283a9bf836fd56ead1e70f12c3994fa1a31ec47
arayhan <rayhanprima99@gmail.com>
2022-11-20 01:37:28 +0700

    [FIX] auth
    - update auth endpoint from partner to user

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,7 +3,7 @@ import { http } from './http';
 export const authLogin = async (params) => {
 	try {
 		const request = { ...params };
-		const response = await http.post('/partner/login', request);
+		const response = await http.post('/user/login', request);
 		return { success: response.data.success, payload: response.data.data };
 	} catch (error) {
 		return { success: false, payload: error };
