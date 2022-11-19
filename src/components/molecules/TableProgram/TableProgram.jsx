5b7c94ae6e916f7eba3c44be476d5f933ed8eb12
arayhan <rayhanprima99@gmail.com>
2022-11-20 04:31:13 +0700

    [REFACTOR] TableProgram : tidy up using program store

--- src/components/molecules/TableProgram/TableProgram.jsx
+++ src/components/molecules/TableProgram/TableProgram.jsx
@@ -4,8 +4,7 @@ import { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 export const TableProgram = ({ selectedCategory }) => {
-	const { fetchingProgramList, programList } = useProgramStore();
-	const { getProgramList } = useProgramStore();
+	const { programList, fetchingProgramList, getProgramList } = useProgramStore();
 
 	const [data, setData] = useState([]);
 
