a680859a085f1812db3bbd44491878e5027381da
arayhan <rayhanprima99@gmail.com>
2022-11-20 10:07:10 +0700

    [FIX] ProgramDetail : fix skeleton loader layout

--- src/pages/Program/ProgramDetail.jsx
+++ src/pages/Program/ProgramDetail.jsx
@@ -88,16 +88,18 @@ const ProgramDetail = () => {
 
 const ProgramDetailSkeleton = () => (
 	<div className="grid grid-cols-12 gap-6">
-		<div className="col-span-3 bg-white p-4 rounded-md">
-			<div className="space-y-3 flex flex-col">
-				<Skeleton width={200} height={20} />
-				<hr />
-				<div className="flex items-center justify-center">
-					<Skeleton className="w-52 h-52 rounded-full" />
+		{[1, 2, 3].map((item) => (
+			<div key={item} className="col-span-12 md:col-span-4 bg-white p-4 rounded-md">
+				<div className="space-y-3 flex flex-col">
+					<Skeleton width={200} height={20} />
+					<hr />
+					<div className="flex items-center justify-center">
+						<Skeleton className="w-48 h-48 md:w-52 md:h-52 rounded-full" />
+					</div>
 				</div>
 			</div>
-		</div>
-		<div className="col-span-9 bg-white p-5 md:p-8 rounded-md">
+		))}
+		<div className="col-span-12 bg-white p-5 md:p-8 rounded-md">
 			<div className="grid grid-cols-12 gap-x-4 gap-y-2">
 				<Skeleton inline containerClassName="col-span-4 md:col-span-3 lg:col-span-2" />
 				<Skeleton inline containerClassName="col-span-8 md:col-span-9 lg:col-span-10" />
