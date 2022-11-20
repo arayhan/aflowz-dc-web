5b7a9d7bfba928da6ad7465f35dec6f17b4e9b7e
arayhan <rayhanprima99@gmail.com>
2022-11-20 07:32:53 +0700

    [CHORE] HomeFeatureList : responsive

--- src/components/sections/HomeFeature/HomeFeatureList.jsx
+++ src/components/sections/HomeFeature/HomeFeatureList.jsx
@@ -4,12 +4,12 @@ import { MENUS } from '@/utils/constants';
 export const HomeFeatureList = () => {
 	return (
 		<div className="flex justify-center">
-			<div className="max-w-screen-xl w-full grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-3 md:gap-5">
+			<div className="max-w-screen-2xl w-full flex flex-wrap justify-center gap-3 md:gap-5">
 				{MENUS.map((menu) => (
 					<Link
 						key={menu.path}
 						to={menu.path}
-						className="bg-white py-2 bg-opacity-10 shadow-md rounded-md hover:bg-opacity-20 cursor-pointer text-white text-opacity-50 transition-all"
+						className="w-24 sm:w-28 md:w-32 bg-white py-2 bg-opacity-10 shadow-md rounded-md hover:bg-opacity-20 cursor-pointer text-white text-opacity-50 transition-all"
 					>
 						<div className="px-5">
 							<img className="w-full" src={menu.icon} alt="" />
