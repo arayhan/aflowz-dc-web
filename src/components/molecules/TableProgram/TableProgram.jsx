5be464932ff9413de60c5eccadbb73818d0c94fe
arayhan <rayhanprima99@gmail.com>
2022-11-22 00:01:13 +0700

    [FEAT] program
    - add button create program & upload xls

--- src/components/molecules/TableProgram/TableProgram.jsx
+++ src/components/molecules/TableProgram/TableProgram.jsx
@@ -1,6 +1,7 @@
 import { Table } from '@/components/atoms';
 import { useProgramStore } from '@/store';
 import { useEffect, useState, useMemo } from 'react';
+import { SiGooglesheets } from 'react-icons/si';
 import { Link } from 'react-router-dom';
 
 export const TableProgram = ({ selectedCategory }) => {
@@ -66,12 +67,24 @@ export const TableProgram = ({ selectedCategory }) => {
 
 	return (
 		<div className="bg-white rounded-md shadow-md">
-			<div className="p-6 flex items-center justify-between">
+			<div className="p-6 flex flex-col md:flex-row gap-4 items-end md:items-center justify-between">
 				<div>
 					<div className="text-lg font-extralight">{selectedCategory?.name || 'All Category'}</div>
 					<div className="text-sm text-gray-400">
 						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium animi dolorum eveniet.
 					</div>
+				</div>
+				<div className="w-full lg:w-1/4 flex flex-col sm:justify-end sm:flex-row gap-3">
+					<button className="bg-green-500 hover:bg-green-600 space-x-1 text-white px-5 py-3 flex items-center justify-center rounded-md transition-all">
+						<SiGooglesheets />
+						<span className="text-sm">Upload XLS</span>
+					</button>
+					<Link
+						to="/program/create"
+						className="block bg-blue-500 hover:bg-blue-600 space-x-1 text-white px-5 py-3 rounded-md transition-all text-center text-sm"
+					>
+						<span>Create Program</span>
+					</Link>
 				</div>
 			</div>
 			<div className="overflow-x-scroll">
