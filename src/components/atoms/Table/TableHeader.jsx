368dd787e5cd04a5e66b6cb58291d5b3fc9f5f17
arayhan <rayhanprima99@gmail.com>
2022-11-26 03:05:02 +0700

    [FEAT] Upload XLS Program
    - show upload xls modal

--- src/components/atoms/Table/TableHeader.jsx
+++ src/components/atoms/Table/TableHeader.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SiGooglesheets } from 'react-icons/si';
 import { Link, useLocation } from 'react-router-dom';
+import { Modal } from '../Modal/Modal';
 
 export const TableHeader = ({ title, description, isReadonly }) => {
 	const location = useLocation();
 
+	const [showUploadForm, setShowUploadForm] = useState(false);
+
 	return (
 		<div className="flex flex-col md:flex-row gap-4 items-end md:items-center justify-between">
+			{showUploadForm && (
+				<Modal
+					title="Upload XLS Program"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, eligendi."
+					onClose={() => setShowUploadForm(false)}
+				>
+					<label
+						htmlFor="uploadXls"
+						className="block border border-dashed text-center px-4 py-16 text-gray-300 rounded-md cursor-pointer hover:bg-gray-100"
+					>
+						<div>UPLOAD HERE</div>
+						<input type="file" id="uploadXls" className="hidden" />
+					</label>
+				</Modal>
+			)}
 			<div>
 				<div className="text-xl font-light">{title}</div>
 				<div className="text-sm text-gray-400">{description}</div>
 			</div>
 			{!isReadonly && (
 				<div className="w-full xl:w-1/3 flex flex-col sm:justify-end sm:flex-row gap-3">
-					<button className="bg-green-500 hover:bg-green-600 w-full lg:w-auto space-x-2 text-white px-5 py-3 flex items-center justify-center rounded-sm transition-all">
+					<button
+						className="bg-green-500 hover:bg-green-600 w-full lg:w-auto space-x-2 text-white px-5 py-3 flex items-center justify-center rounded-sm transition-all"
+						onClick={() => setShowUploadForm(true)}
+					>
 						<span className="w-4">
 							<SiGooglesheets size={16} />
 						</span>
