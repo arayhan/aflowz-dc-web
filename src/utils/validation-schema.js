b26e4ea07b5923fe186ee4e5e73c0bc83091d038
arayhan <rayhanprima99@gmail.com>
2022-11-27 06:03:19 +0700

    [CHORE] Form Program : add validation for pic eksternal

--- src/utils/validation-schema.js
+++ src/utils/validation-schema.js
@@ -10,16 +10,13 @@ export const formProgramSchema = yup.object().shape(
 		program_category_id: yup.number().required('Mitra wajib diisi'),
 		name: yup.string().required('Nama program wajib diisi'),
 		periode: yup.number('Periode harus berupa angka').required('Periode wajib diisi'),
-		pic_staff_id: yup.number().required('PIC wajib diisi'),
-		pic_mobile: yup.string().when('pic_mobile', (val) => {
-			return val?.length > 0
-				? yup
-						.string()
-						.min(8, 'Nomor tidak valid')
-						.matches(/^[0-9]*$/, 'Nomor tidak valid')
-						.required('Nomor PIC wajib diisi')
-				: yup.string().notRequired();
-		})
+		pic_staff_id: yup.number().required('PIC internal wajib diisi'),
+		pic: yup.string().required('PIC eksternal wajib diisi'),
+		pic_mobile: yup
+			.string()
+			.required('Nomor PIC eksternal wajib diisi')
+			.matches(/^[0-9]*$/, 'Nomor tidak valid')
+			.min(8, 'Minimal 8 angka')
 	},
 	['pic_mobile', 'pic_mobile']
 );
