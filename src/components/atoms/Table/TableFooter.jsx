f31b8638b7787e94cf036f3f1f014537d8d01d9f
arayhan <rayhanprima99@gmail.com>
2022-11-27 14:28:41 +0700

    [CHORE] add pagination component on table footer

--- src/components/atoms/Table/TableFooter.jsx
+++ src/components/atoms/Table/TableFooter.jsx
@@ -1,12 +1,29 @@
+import ReactPaginate from 'react-paginate';
 import { TablePerPage } from './TablePerPage';
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
 
 export const TableFooter = () => {
 	return (
-		<div className="flex flex-col md:flex-row gap-4 items-end md:items-center justify-between">
-			<div className="w-1/2">
+		<div className="flex flex-col sm:flex-row gap-6 sm:gap-4 items-center justify-between">
+			<div className="sm:w-1/2">
 				<TablePerPage />
 			</div>
-			<div>Coming Soon</div>
+			<div>
+				<ReactPaginate
+					marginPagesDisplayed={1}
+					pageRangeDisplayed={2}
+					previousLabel={<GrFormPrevious />}
+					nextLabel={<GrFormNext />}
+					pageCount={20}
+					containerClassName="flex text-sm space-x-2 items-center justify-center"
+					activeLinkClassName="bg-gray-200 text-gray-700"
+					breakLinkClassName="block text-gray-500 hover:bg-gray-100 rounded-sm px-3 py-2"
+					pageLinkClassName="block text-gray-500 hover:bg-gray-100 rounded-sm px-3 py-2"
+					previousLinkClassName="block text-gray-500 rounded-sm p-3 hover:bg-gray-100"
+					nextLinkClassName="block text-gray-500 rounded-sm p-3 hover:bg-gray-100"
+					disabledLinkClassName="text-gray-100"
+				/>
+			</div>
 		</div>
 	);
 };
--- src/components/atoms/Table/TablePerPage.jsx
+++ src/components/atoms/Table/TablePerPage.jsx
@@ -5,11 +5,9 @@ import { forwardRef } from 'react';
 export const TablePerPage = forwardRef(({ error, value, disabled, onChange, ...props }, ref) => {
 	const { perPage, perPageOptions: options, setPerPage } = useTableContext();
 
-	console.log({ perPage });
-
 	return (
 		<div className="flex items-center space-x-2">
-			<InputLabel text="Per Page" name={props.name} />
+			<InputLabel className="text-sm" text="Per Page" name={props.name} />
 			<select
 				ref={ref}
 				className="border-gray-400 rounded-md text-sm"
