0c6ecc5896198c81d8090df724fc688394f7c299
arayhan <rayhanprima99@gmail.com>
2022-11-30 06:57:04 +0700

    [FIX] Program - Select Program Category : can select and unselect selected program category

--- src/pages/Program/Program.jsx
+++ src/pages/Program/Program.jsx
@@ -4,14 +4,16 @@ import { useState } from 'react';
 const Program = () => {
 	const [selectedCategory, setSelectedCategory] = useState(null);
 
+	const handleSelectCategory = (category) => {
+		if (category.id === selectedCategory?.id) setSelectedCategory(null);
+		else setSelectedCategory(category);
+	};
+
 	return (
 		<div>
 			<BannerFeature title="Program" description="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
 			<div className="bg-gray-100">
-				<SectionSelectProgramCategory
-					selectedCategory={selectedCategory}
-					onSelectCategory={(category) => setSelectedCategory(category)}
-				/>
+				<SectionSelectProgramCategory selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
 				<div className="py-6 container">
 					<TableProgram selectedCategory={selectedCategory} />
 				</div>
