89a2267d7541a6036bcf76e6e083fee8c0db309c
arayhan <rayhanprima99@gmail.com>
2022-11-30 19:57:08 +0700

    [CHORE] Table Program & Table Header
    - add custom displayed columns
    - add isReadonly props
    - add isShowFooter props
    - add custom table title
    - add mainRoute props

--- src/components/atoms/Table/TableHeader.jsx
+++ src/components/atoms/Table/TableHeader.jsx
@@ -7,8 +7,10 @@ export const TableHeader = ({
 	feature,
 	title,
 	description,
+	mainRoute,
 	isReadonly,
 	showButtonCreate,
+	showButtonSeeAll,
 	showButtonUploadPartnerSheet
 }) => {
 	const location = useLocation();
@@ -22,29 +24,35 @@ export const TableHeader = ({
 				<div className="text-xl font-light">{title}</div>
 				<div className="text-sm text-gray-400">{description}</div>
 			</div>
-			{!isReadonly && (
-				<div className="w-full xl:w-1/3 flex flex-col md:justify-end md:flex-row gap-3">
-					{showButtonUploadPartnerSheet && (
-						<button
-							className="bg-green-500 hover:bg-green-600 w-full lg:w-auto space-x-2 text-white px-5 py-3 flex items-center justify-center rounded-sm transition-all"
-							onClick={() => setShowModalUploadPartnerSheet(true)}
-						>
-							<span className="w-4">
-								<SiGooglesheets size={16} />
-							</span>
-							<span className="text-sm">Upload Sheet Penerima Program</span>
-						</button>
-					)}
-					{showButtonCreate && (
-						<Link
-							to={`${location.pathname}/create`}
-							className="block bg-blue-500 hover:bg-blue-600 w-full lg:w-auto space-x-2 text-white px-5 py-3 rounded-sm transition-all text-center text-sm"
-						>
-							<span>Create {feature}</span>
-						</Link>
-					)}
-				</div>
-			)}
+			<div className="w-full xl:w-1/3 flex flex-col md:justify-end md:flex-row gap-3">
+				{showButtonSeeAll && (
+					<Link
+						to={`${mainRoute}`}
+						className="block bg-blue-500 hover:bg-blue-600 w-full lg:w-auto space-x-2 text-white px-5 py-3 rounded-sm transition-all text-center text-sm"
+					>
+						<span>Lihat Semua</span>
+					</Link>
+				)}
+				{!isReadonly && showButtonUploadPartnerSheet && (
+					<button
+						className="bg-green-500 hover:bg-green-600 w-full lg:w-auto space-x-2 text-white px-5 py-3 flex items-center justify-center rounded-sm transition-all"
+						onClick={() => setShowModalUploadPartnerSheet(true)}
+					>
+						<span className="w-4">
+							<SiGooglesheets size={16} />
+						</span>
+						<span className="text-sm">Upload Sheet Penerima Program</span>
+					</button>
+				)}
+				{!isReadonly && showButtonCreate && (
+					<Link
+						to={`${location.pathname}/create`}
+						className="block bg-blue-500 hover:bg-blue-600 w-full lg:w-auto space-x-2 text-white px-5 py-3 rounded-sm transition-all text-center text-sm"
+					>
+						<span>Create {feature}</span>
+					</Link>
+				)}
+			</div>
 		</div>
 	);
 };
