889e0d143293bab548bff52d91e0c150a5a4ee20
arayhan <rayhanprima99@gmail.com>
2022-12-04 03:43:02 +0700

    [CHORE] Card : update action button on card component

--- src/components/atoms/Card/Card.jsx
+++ src/components/atoms/Card/Card.jsx
@@ -4,14 +4,19 @@ import { ButtonAction } from '../Button/ButtonAction';
 export const Card = ({ title, description, className, bodyClassName, children, isInDetail, linkRoute }) => {
 	return (
 		<div className={className}>
-			<div className="flex items-center justify-between">
-				<div className="p-4 space-y-2">
+			<div className="md:flex items-center justify-between p-4">
+				<div className="space-y-2">
 					{title && <div className="font-light text-xl">{title}</div>}
 					{description && <div className="text-sm text-gray-400">{description}</div>}
 				</div>
 				{isInDetail && (
-					<div className="p-4 space-y-2">
-						<ButtonAction linkTo={linkRoute} text={'Update'} action={ACTION_TYPES.UPDATE} className={'px-7 py-4'} />
+					<div>
+						<ButtonAction
+							action={ACTION_TYPES.UPDATE}
+							linkTo={linkRoute}
+							className={'w-full md:w-auto text-base px-5 py-3 rounded-md'}
+							text="Update"
+						/>
 					</div>
 				)}
 			</div>
