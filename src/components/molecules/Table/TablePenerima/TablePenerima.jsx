4f3822cc1da3b3937c4278fa4ccd2b50bdb448f6
arayhan <rayhanprima99@gmail.com>
2022-12-06 02:19:18 +0700

    [CHORE] Table Penerima
    - add is_staff: false as default params

--- src/components/molecules/Table/TablePenerima/TablePenerima.jsx
+++ src/components/molecules/Table/TablePenerima/TablePenerima.jsx
@@ -138,7 +138,7 @@ export const TablePenerima = ({
 
 	useEffect(() => {
 		const offsetResult = (page - 1) * perPage;
-		const defaultParams = { limit: perPage, offset: offsetResult, is_follower: false };
+		const defaultParams = { limit: perPage, offset: offsetResult, is_follower: false, is_staff: false };
 
 		if (page > pageCount) setPage(pageCount);
 		else {
