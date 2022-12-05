3664df4d5394122468f95e3a43706e5249252938
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-12-05 15:46:06 +0700

    REFACTOR: change photo button

--- src/pages/Staff/StaffDetail.jsx
+++ src/pages/Staff/StaffDetail.jsx
@@ -12,6 +12,7 @@ import { useEffect, useRef, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import { Button, ButtonAction, InputTextInfo } from '@/components/atoms';
 import { ACTION_TYPES } from '@/utils/constants';
+import { MdAddAPhoto } from 'react-icons/md';
 
 const StaffDetail = () => {
 	const { isSystem, isAdmin } = useAuthStore();
@@ -103,7 +104,7 @@ const StaffDetail = () => {
 									</div>
 									<hr />
 									<div className="p-5 rounded-md my-2 flex-col">
-										<div className="w-full flex justify-center mb-5">
+										<div className="w-full flex justify-center">
 											{staff?.image_url || editPicture !== '' ? (
 												<img src={editPicture ? editPicture : staff?.image_url} className="w-52" />
 											) : (
@@ -113,43 +114,52 @@ const StaffDetail = () => {
 												/>
 											)}
 										</div>
-										{showError && <p className="w-full flex justify-center mb-5 text-red-500">Max. Photo Size 500kb</p>}
-										{(isAdmin || isSystem) && editPicture !== '' && showButton ? (
-											<div className="w-full flex justify-center mb-5">
-												<div className="w-52 flex justify-evenly">
-													<Button variant={'success'} type="button" onClick={handleSavePicture} className="px-5 py-2">
-														Save
-													</Button>
+										<div className="w-full flex justify-center">
+											{showError && (
+												<p className="w-full flex justify-center mb-5 text-red-500">Max. Photo Size 500kb</p>
+											)}
+											{(isAdmin || isSystem) && editPicture !== '' && showButton ? (
+												<div className="w-52 flex justify-center mb-5 mt-2">
+													<div className="w-52 flex justify-evenly">
+														<Button variant={'success'} type="button" onClick={handleSavePicture} className="px-5 py-2">
+															Save
+														</Button>
+														<Button
+															type="button"
+															variant={'danger'}
+															className="px-3 py-2"
+															onClick={() => {
+																setEditPicture('');
+																setShowButton(false);
+															}}
+														>
+															Cancel
+														</Button>
+													</div>
+												</div>
+											) : (
+												<div className="w-52 flex justify-center mb-5 mt-2">
 													<Button
 														type="button"
-														variant={'danger'}
-														className="px-3 py-2"
-														onClick={() => {
-															setEditPicture('');
-															setShowButton(false);
-														}}
+														onClick={() => fileRef.current.click()}
+														variant={'primary'}
+														className="p-4 rounded-full"
 													>
-														Cancel
+														<MdAddAPhoto />
+														<input
+															type="file"
+															onChange={(e) => {
+																setGetFileSize(e.target.files[0]);
+																uploadImage(e);
+																setShowButton(true);
+															}}
+															ref={fileRef}
+															hidden
+														/>
 													</Button>
 												</div>
-											</div>
-										) : (
-											<div className="w-full flex justify-center mb-5">
-												<button type="button" onClick={() => fileRef.current.click()}>
-													<span>Change Photo</span>
-													<input
-														type="file"
-														onChange={(e) => {
-															setGetFileSize(e.target.files[0]);
-															uploadImage(e);
-															setShowButton(true);
-														}}
-														ref={fileRef}
-														hidden
-													/>
-												</button>
-											</div>
-										)}
+											)}
+										</div>
 										<div className="overflow-x-auto">
 											<div className="grid grid-cols-12 w-full gap-y-1 text-sm">
 												<InputTextInfo tag={'NIK'} value={staff?.nik_number || 'Belum Tercantum'} />
