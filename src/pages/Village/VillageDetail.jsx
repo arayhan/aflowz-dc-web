ff48e17ee3614c186324a666463c0a9dbf4d7b3f
arayhan <rayhanprima99@gmail.com>
2022-12-06 17:27:24 +0700

    [CHORE] VillageDetail
    - set penerima params, is_staff true to TablePenerima on village detail

--- src/pages/Village/VillageDetail.jsx
+++ src/pages/Village/VillageDetail.jsx
@@ -17,7 +17,7 @@ const VillageDetail = () => {
 
 	const { villageDetail, fetchingVillageDetail, getVillageDetail } = useVillageStore();
 
-	const [tableParams] = useState({ village_id: villageID });
+	const [tableParams] = useState({ village_id: villageID, is_staff: true });
 
 	useEffect(() => {
 		if (villageID) getVillageDetail(villageID);
