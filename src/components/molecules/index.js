a7b0a25ae4a063d10338de9f0314647fd2481a76
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-12-15 12:18:16 +0700

    FEAT: add carousel component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -6,6 +6,8 @@ export * from './Banner/BannerHome/BannerHome';
 export * from './Button/ButtonBack/ButtonBack';
 export * from './Button/ButtonProfileHeader/ButtonProfileHeader';
 
+export * from './Carousel/Carousel';
+
 export * from './Card/CardDetailTotal/CardDetailTotal';
 export * from './Card/CardPenerimaProgramByGender/CardPenerimaProgramByGender';
 
