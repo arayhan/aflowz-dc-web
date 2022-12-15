7beccd1fbd1da9b44d245f69de90c68ac874b80a
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-12-16 01:33:30 +0700

    FIX: home banner carousel

--- src/components/molecules/Carousel/Carousel.jsx
+++ src/components/molecules/Carousel/Carousel.jsx
@@ -1,27 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { CarouselData } from './CarouselData';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Carousel.css';
 
 export const CarouselComp = () => {
+	const settings = {
+		dots: true,
+		className: 'center',
+		centerMode: true,
+		infinite: true,
+		autoplay: true,
+		speed: 1000,
+		autoplaySpeed: 5000,
+		cssEase: 'linear',
+		adaptiveHeight: false,
+		focusOnSelect: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+					dots: true,
+					className: 'center',
+					centerMode: true,
+					infinite: true,
+					autoplay: true,
+					speed: 1000,
+					autoplaySpeed: 5000,
+					cssEase: 'linear',
+					adaptiveHeight: false,
+					focusOnSelect: true,
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	};
+
 	return (
-		<div className="mt-8">
-			<Carousel
-				autoPlay={true}
-				interval={'5000'}
-				infiniteLoop={true}
-				swipeable={true}
-				showArrows={true}
-				showThumbs={false}
-			>
+		<div className="py-10 h-fit">
+			<Slider {...settings}>
 				{CarouselData.length > 0
 					? CarouselData.map((data, index) => (
-							<div key={index} className="h-full">
-								<img src={data.image} className="h-full rounded-lg" />
+							<div key={index} className="h-full px-1 sm:px-5 rounded-xl">
+								<img src={data.image} className="h-full rounded-xl" />
 							</div>
 					  ))
 					: null}
-			</Carousel>
+			</Slider>
 		</div>
 	);
 };
