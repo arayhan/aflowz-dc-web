7f2d240270410b4630f7904bb90879213e38c824
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-12-16 20:50:10 +0700

    REFACTOR: home page menu section

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -31,15 +31,15 @@ export const NEGATIVE_CASE_TYPES = {
 export const PER_PAGE_OPTIONS = [5, 10, 20, 50, 100];
 
 export const MENUS = [
-	{ title: 'Mitra', icon: require('@/images/icons/Icon_Home/Mitra.svg').default, path: '/mitra' },
-	{ title: 'Program', icon: require('@/images/icons/Icon_Home/Program.svg').default, path: '/program' },
-	{ title: 'Institusi', icon: require('@/images/icons/Icon_Home/Konstitusi.svg').default, path: '/institusi' },
-	{ title: 'Penerima', icon: require('@/images/icons/Icon_Home/Penerima.svg').default, path: '/penerima' },
-	{ title: 'Desa', icon: require('@/images/icons/Icon_Home/Desa.svg').default, path: '/village' },
-	{ title: 'Kota', icon: require('@/images/icons/Icon_Home/Kota.svg').default, path: '/city' },
-	{ title: 'Tim Internal', icon: require('@/images/icons/Icon_Home/Staff.svg').default, path: '/staff' },
-	{ title: 'Dapil', icon: require('@/images/icons/Icon_Home/Dapil.svg').default, path: '/dapil' },
-	{ title: 'Absensi', icon: require('@/images/icons/Icon_Home/Absensi.svg').default, path: '/absensi' },
-	{ title: 'Inventory', icon: require('@/images/icons/Icon_Home/Inventory.svg').default, path: '/stockiest' },
-	{ title: 'Kegiatan', icon: require('@/images/icons/Icon_Home/Activity.svg').default, path: '/activity' }
+	{ id: 1, title: 'Mitra', icon: require('@/images/icons/Icon_Home/Mitra.svg').default, path: '/mitra' },
+	{ id: 2, title: 'Program', icon: require('@/images/icons/Icon_Home/Program.svg').default, path: '/program' },
+	{ id: 3, title: 'Institusi', icon: require('@/images/icons/Icon_Home/Konstitusi.svg').default, path: '/institusi' },
+	{ id: 4, title: 'Tim Internal', icon: require('@/images/icons/Icon_Home/Staff.svg').default, path: '/staff' },
+	{ id: 5, title: 'Absensi', icon: require('@/images/icons/Icon_Home/Absensi.svg').default, path: '/absensi' },
+	{ id: 6, title: 'Penerima', icon: require('@/images/icons/Icon_Home/Penerima.svg').default, path: '/penerima' },
+	{ id: 7, title: 'Kegiatan', icon: require('@/images/icons/Icon_Home/Activity.svg').default, path: '/activity' },
+	{ id: 8, title: 'Inventory', icon: require('@/images/icons/Icon_Home/Inventory.svg').default, path: '/stockiest' },
+	{ id: 9, title: 'Dapil', icon: require('@/images/icons/Icon_Home/Dapil.svg').default, path: '/dapil' },
+	{ id: 10, title: 'Desa', icon: require('@/images/icons/Icon_Home/Desa.svg').default, path: '/village' },
+	{ id: 11, title: 'Kota', icon: require('@/images/icons/Icon_Home/Kota.svg').default, path: '/city' }
 ];
