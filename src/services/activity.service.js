f50c42378ef958105abf42c974bac950ca6caedb
arayhan <rayhanprima99@gmail.com>
2022-12-17 19:16:31 +0700

    [FEAT] Kegiatan
    - add all related activity service

--- src/services/activity.service.js
+++ src/services/activity.service.js
@@ -1,6 +1,9 @@
 import { objectToQueryString } from '@/utils/helpers';
 import { http } from './http';
 
+// ==================================
+// Activity
+// ==================================
 export const getActivityItem = async (activityID) => {
 	try {
 		const response = await http.get(`/activity/${activityID}`);
@@ -14,35 +17,6 @@ export const getActivityList = async (params) => {
 	try {
 		const queryParams = objectToQueryString(params);
 		const response = await http.get('/activity' + queryParams);
-		return { success: response.data.success, payload: response.data.data };
-	} catch (error) {
-		return { success: false, payload: error };
-	}
-};
-
-export const getPromiseList = async (params) => {
-	try {
-		const queryParams = objectToQueryString(params);
-		const response = await http.get('activity/promise' + queryParams);
-		return { success: response.data.success, payload: response.data.data };
-	} catch (error) {
-		return { success: false, payload: error };
-	}
-};
-
-export const getActivityDetailItem = async (activityID) => {
-	try {
-		const response = await http.get('/activity/detail/' + activityID);
-		return { success: response.data.success, payload: response.data.data };
-	} catch (error) {
-		return { success: false, payload: error };
-	}
-};
-
-export const getActivityDetailList = async (params) => {
-	try {
-		const queryParams = objectToQueryString(params);
-		const response = await http.get('/activity/detail/' + queryParams);
 		return { success: response.data.success, payload: response.data.data };
 	} catch (error) {
 		return { success: false, payload: error };
@@ -88,6 +62,137 @@ export const updateActivity = async (activityID, params) => {
 export const deleteActivity = async (activityID) => {
 	try {
 		const response = await http.delete(`/activity/${activityID}`);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+// ==================================
+// Activity Detail
+// ==================================
+
+export const getActivityDetailItem = async (activityID) => {
+	try {
+		const response = await http.get('/activity/detail/' + activityID);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const getActivityDetailList = async (params) => {
+	try {
+		const queryParams = objectToQueryString(params);
+		const response = await http.get('/activity/detail/' + queryParams);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const createActivityDetail = async (params) => {
+	const request = {
+		activity_id: params?.activity_id || 0,
+		description: params?.description || '',
+		activity_date: params?.activity_date || '',
+		pic: params?.pic || '',
+		pic_mobile: params?.pic_mobile || '',
+		pic_staff_id: params?.pic_staff_id || 0,
+		promise_datas: params?.promise_datas || []
+	};
+
+	try {
+		const response = await http.post('/activity/detail', request);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const updateActivityDetail = async (activityDetailID, params) => {
+	const request = {
+		activity_id: params?.activity_id || 0,
+		description: params?.description || '',
+		activity_date: params?.activity_date || '',
+		pic: params?.pic || '',
+		pic_mobile: params?.pic_mobile || '',
+		pic_staff_id: params?.pic_staff_id || 0,
+		promise_datas: params?.promise_datas || []
+	};
+
+	try {
+		const response = await http.put(`/activity/detail/${activityDetailID}`, request);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const deleteActivityDetail = async (activityDetailID) => {
+	try {
+		const response = await http.delete(`/activity/detail/${activityDetailID}`);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+// ==================================
+// Activity Promise
+// ==================================
+export const getActivityPromiseItem = async (activityPromiseID) => {
+	try {
+		const response = await http.get('/activity/promise/' + activityPromiseID);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const getActivityPromiseList = async (params) => {
+	try {
+		const queryParams = objectToQueryString(params);
+		const response = await http.get('activity/promise' + queryParams);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const createActivityPromise = async (params) => {
+	const request = {
+		activity_detail_id: params?.activity_detail_id || 0,
+		name: params?.name || '',
+		realization: params?.realization || false
+	};
+
+	try {
+		const response = await http.post('/activity/promise', request);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const updateActivityPromise = async (activityPromiseID, params) => {
+	const request = {
+		activity_detail_id: params?.activity_detail_id || 0,
+		name: params?.name || '',
+		realization: params?.realization || false
+	};
+
+	try {
+		const response = await http.put(`/activity/promise/${activityPromiseID}`, request);
+		return { success: response.data.success, payload: response.data.data };
+	} catch (error) {
+		return { success: false, payload: error };
+	}
+};
+
+export const deleteActivityPromise = async (activityPromiseID) => {
+	try {
+		const response = await http.delete(`/activity/promise/${activityPromiseID}`);
 		return { success: response.data.success, payload: response.data.data };
 	} catch (error) {
 		return { success: false, payload: error };
