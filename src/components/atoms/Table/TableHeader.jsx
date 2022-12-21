638c7b45acd1802c9884702c6d4810a607030983
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-12-21 20:56:09 +0700

    FIX: set responsiveness on title table header

--- src/components/atoms/Table/TableHeader.jsx
+++ src/components/atoms/Table/TableHeader.jsx
@@ -29,7 +29,7 @@ export const TableHeader = ({
 			{showModalUploadSheetPenerima && (
 				<ModalUploadSheetPenerima onClose={() => setShowModalUploadSheetPenerima(false)} />
 			)}
-			<div className="w-1/3">
+			<div className="w-full xl:w-1/3">
 				<div className="text-xl font-light transform: capitalize">{title}</div>
 				{/* <div className="text-sm text-gray-400">{description}</div> */}
 			</div>
