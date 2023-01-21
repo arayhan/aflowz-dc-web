bfa41317f1454519f632ff3c67485a3d1f51df59
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-01-21 14:29:09 +0700

    [CHORE] add shadow on SiteHeader

--- src/components/layouts/SiteLayout/SiteHeader/SiteHeader.jsx
+++ src/components/layouts/SiteLayout/SiteHeader/SiteHeader.jsx
@@ -5,10 +5,10 @@ import siteHeaderLogo from '@/images/icons/3.png';
 
 export const SiteHeader = () => {
 	return (
-		<div className="bg-white">
+		<div className="bg-white shadow-md">
 			<div className="container">
 				<div className="flex items-center justify-between">
-					<Link to={'/'} className="text-white font-extralight text-lg md:text-xl">
+					<Link to={'/'} className="text-lg text-white font-extralight md:text-xl">
 						<img src={siteHeaderLogo} className="max-w-[5rem]" />
 					</Link>
 					<ButtonProfileHeader />
