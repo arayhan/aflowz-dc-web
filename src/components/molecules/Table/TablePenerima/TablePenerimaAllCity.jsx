35380880080d4b4bc99c3ef77580c7e478e03001
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-01-29 00:17:26 +0700

    [FIX] TablePenerimaAllCity on Home
    - move city routo inside dapil

--- src/components/molecules/Table/TablePenerima/TablePenerimaAllCity.jsx
+++ src/components/molecules/Table/TablePenerima/TablePenerimaAllCity.jsx
@@ -16,20 +16,20 @@ export const TablePenerimaAllCity = () => {
 	}, [penerimaAllCity]);
 
 	return (
-		<div className="bg-gray-100 py-5">
+		<div className="py-5 bg-gray-100">
 			<div className="container px-14">
-				<div className="overflow-y-auto container flex-col border py-5 rounded-lg bg-white shadow-lg">
-					<p className="text-xl lg:text-3xl font-extralight text-center pb-5">Daftar Penerima di Provinsi Bengkulu</p>
-					<div className="h-44 sm:h-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 pb-5">
+				<div className="container flex-col py-5 overflow-y-auto bg-white border rounded-lg shadow-lg">
+					<p className="pb-5 text-xl text-center lg:text-3xl font-extralight">Daftar Penerima di Provinsi Bengkulu</p>
+					<div className="grid grid-cols-1 pb-5 h-44 sm:h-fit sm:grid-cols-2 md:grid-cols-3">
 						{data.map((val) => {
 							return (
 								<Link
-									to={`/city/${val.id}`}
+									to={`/dapil/city/${val.id}`}
 									key={val.id}
-									className="flex justify-between mx-2 my-2 border shadow-lg py-2 px-2 rounded-md cursor-pointer hover:bg-gray-100"
+									className="flex justify-between px-2 py-2 mx-2 my-2 border rounded-md shadow-lg cursor-pointer hover:bg-gray-100"
 								>
-									<p className="text-xs lg:text-base font-light">{val.name}</p>
-									<p className="text-xs lg:text-base font-light">Penerima ({val.total_penerima} Orang)</p>
+									<p className="text-xs font-light lg:text-base">{val.name}</p>
+									<p className="text-xs font-light lg:text-base">Penerima ({val.total_penerima} Orang)</p>
 								</Link>
 							);
 						})}
