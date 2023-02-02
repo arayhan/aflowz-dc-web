858607268216f9cd4114be93ac2dff7edf224937
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-02-03 01:09:25 +0700

    fix: Feedback 6 Januari 2022
    - fix : tambah field status (udah dilakukan/belum dilakukan) di kegiatan

--- src/pages/Staff/StaffListPage.jsx
+++ src/pages/Staff/StaffListPage.jsx
@@ -1,12 +1,14 @@
-import { BannerFeature } from '@/components/molecules';
+import { BannerFeature, CardOrganizationStructure } from '@/components/molecules';
 import { TableStaff } from '@/components/molecules/Table/TableStaff/TableStaff';
+import { ORGANIZATION_TYPE } from '@/utils/constants';
 
 const StaffListPage = () => {
 	return (
 		<div>
 			<BannerFeature title="List Tim Internal" />
 			<div className="bg-gray-100">
-				<div className="container py-6">
+				<div className="container py-6 space-y-4">
+					<CardOrganizationStructure type={ORGANIZATION_TYPE.TIM_INTERNAL} />
 					<TableStaff />
 				</div>
 			</div>
