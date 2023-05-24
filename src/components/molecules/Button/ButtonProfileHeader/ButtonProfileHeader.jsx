a38aa9ef5471996d380ca7cc058ab8a77090836c
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-05-24 22:59:26 +0700

    [CHORE] update z-index of siteheader nav profile

--- src/components/molecules/Button/ButtonProfileHeader/ButtonProfileHeader.jsx
+++ src/components/molecules/Button/ButtonProfileHeader/ButtonProfileHeader.jsx
@@ -18,25 +18,25 @@ export const ButtonProfileHeader = () => {
 	const handleLogout = () => authLogout();
 
 	return (
-		<div className="relative flex items-center space-x-3 py-2 md:py-3">
+		<div className="relative flex items-center py-2 space-x-3 md:py-3">
 			<button
 				ref={dropdownRef}
-				className="flex items-center hover:bg-white hover:bg-opacity-5 px-3 md:px-4 py-2 md:py-3 space-x-2 rounded-md"
+				className="flex items-center px-3 py-2 space-x-2 rounded-md hover:bg-white hover:bg-opacity-5 md:px-4 md:py-3"
 				onClick={() => setIsDropdownOpen(true)}
 			>
 				<img
-					className="w-7 md:w-8 rounded-full"
+					className="rounded-full w-7 md:w-8"
 					src="https://static.wixstatic.com/media/0e8679_e83474054db34680800cb2c1bd81fb72~mv2.png/v1/fill/w_544,h_577,al_c,lg_1,q_85,enc_auto/avatars%20-%20Amanda.png"
 					alt=""
 				/>
-				<div className="text-primary-600 text-sm md:text-base">
+				<div className="text-sm text-primary-600 md:text-base">
 					<span>Welcome, </span> <span className="font-extralight">{profile.username}</span>
 				</div>
 			</button>
 
 			{isDropdownOpen && (
-				<div className="absolute right-0 bottom-0 transform translate-y-full w-48 bg-white rounded-sm shadow-lg overflow-hidden">
-					<div className="flex flex-col divide-y text-xs md:text-sm">
+				<div className="absolute bottom-0 right-0 z-10 w-48 overflow-hidden transform translate-y-full bg-white rounded-sm shadow-lg">
+					<div className="flex flex-col text-xs divide-y md:text-sm">
 						<button className={dropdownItemClasses} onClick={() => navigate('/')}>
 							<RiHomeLine />
 							<span>Home</span>
